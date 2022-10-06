8bd55a248b81b07a2d4e87c663f1d9ea2468318c
msjones3 <cheryl@realsocial.com.au>
2022-10-06 21:55:36 +1000

    update posts

---
+++ pages/posts/_id.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-gray-50 min-h-screen">
+    <div class="container mx-auto pt-8">
+      <h1 class="font-bold text-3xl">{{post.title}}</h1>
+      <p>{{post.description}}</p>
+      <NuxtLink to="/" class="block mt-8">Back</NuxtLink>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const route = useRoute();
+const post = await $fetch(`/api/posts/${route.params.id}`);
+
+</script>
--- server/api/posts.ts
+++ server/api/posts.ts
@@ -1,8 +1,26 @@
 import posts from '../../data/posts.json'
-import { IncomingMessage, ServerResponse } from 'http'
+import { IncomingMessage, ServerResponse } from 'h3'
 
 export default function(req:IncomingMessage, res:ServerResponse){
-  return {
+  const {pathname}  = new URL(req.url, 'http://${req.headers.host}')
+  console.log( pathname )
+  const id= pathname.split('/')[1]; // http://localhost:3000/postsPage/1
+  // that doesn't work. Just return the posts.
+  {
+    return posts
+  } 
+
+}
+
+
+
+/*  THIS WORKED
+
+import posts from '../../data/posts.json'
+import { IncomingMessage, ServerResponse } from 'h3'
+
+export default function(req:IncomingMessage, res:ServerResponse){
+  return{
     posts
   }
-}
+} */
