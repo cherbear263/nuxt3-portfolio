127a2b7d822f28e322ff8cd1fb4ea948aef3b7a2
msjones3 <cheryl@realsocial.com.au>
2022-10-06 21:03:25 +1000

    post api

---
+++ pages/now.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="container mx-auto py-8">
+    <h1 class="font-bold text-3xl">Hello, Nitro</h1>
+    current time is {{time}}
+  </div>
+</template>
+<script setup>
+const time = await $fetch('/api/now');
+</script>
---
+++ pages/postsPage.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="ml-80 mt-20">
+    <h1>Blog Posts</h1>
+    <div class="container mx-auto pt-8">
+      <ul class="list-disc">
+        <li v-for="post in posts" :key="post.id">{{post.title}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script setup>
+const { posts } = await $fetch('/api/posts');
+
+</script>
