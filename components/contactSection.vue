35738bebdcf36946fdaa15b75d68c8bc9a0c566b
msjones3 <cheryl@realsocial.com.au>
2023-05-31 13:08:11 +1000

    add action to form

--- components/contactSection.vue
+++ components/contactSection.vue
@@ -4,7 +4,7 @@
     <p class="text-lg text-gray-300">Hey, I would love to hear from you! Drop me a message below and I'll get back soon.
     </p>
     <form name="form-name" value="website contact form" class="w-full max-w-md mt-10" method="post" data-netlify="true"
-      data-netlify-honeypot="bot-field" autocomplete="off">
+      data-netlify-honeypot="bot-field" autocomplete="off" action="/">
       <div class="mb-4">
         <label class="block mb-2 text-sm font-bold text-gray-300" for="name">Name:</label>
         <input
@@ -12,13 +12,13 @@
           name="name" type="text" id="name" v-model="formValues.name" />
       </div>
       <div class="mb-4">
-        <label class="block mb-2 text-sm font-bold text-gray-300" for="email">some value:</label>
+        <label class="block mb-2 text-sm font-bold text-gray-300" for="email">Email:</label>
         <input
           class="w-full px-3 py-2 leading-tight text-indigo-500 bg-gray-800 border-2 border-indigo-500 rounded-lg appearance-none focus:border-3 focus:border-cyan-500 focus:outline-none focus:bg-cyan-100 focus:text-indigo-800 focus:font-semibold"
           name="email" type="email" id="email" v-model="formValues.email" />
       </div>
       <div class="mb-4">
-        <label class="block mb-2 text-sm font-bold text-gray-300" for="message">Message:</label>
+        <label class="block mb-2 text-sm font-bold text-gray-300" for="message">Your Message:</label>
         <textarea
           class="w-full px-3 py-2 leading-tight text-indigo-500 bg-gray-800 border-2 border-indigo-500 rounded-lg appearance-none focus:border-3 focus:border-cyan-500 focus:outline-none focus:bg-cyan-100 focus:text-indigo-800 focus:font-semibold"
           type="textarea" name="message" id="message" rows="6" v-model="formValues.message"></textarea>
